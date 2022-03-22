a77963347d58a0b0f2acb849b2cbb67a0cae49c3
Dmitrii Trebin <d.trebin@rambler.ru>
2022-03-22 18:29:58 +0300

    new commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
     public double calculate(int weight, double height) {
-        double resHight = height/100;
+        double resHight = height / 100;
         double bmi = weight / (resHight * resHight);
         return bmi;
 
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         BmiService service = new BmiService();
         double resultNorm = service.calculate(86, 186);
 
-        System.out.printf("Индекс массы тела: %5.1f кг/м2 %n", resultNorm );
+        System.out.printf("Индекс массы тела: %5.1f кг/м2 %n", resultNorm);
     }
 }
